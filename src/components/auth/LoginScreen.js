63330919ddf74ea4d5d0c0e1410a9485eb67737a
Jesus Delgado <m6nuel@gmail.com>
2021-09-28 13:55:29 -0300

    index actualizado

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -13,8 +13,8 @@ export const LoginScreen = () => {
     const { loading } = useSelector(state => state.ui)
 
     const [ formValues, handleInputChange ] = useForm({
-        email: 'jesus@gmail.com',
-        password: '123456'
+        email: '',
+        password: ''
     });
 
     const { email, password } = formValues;
